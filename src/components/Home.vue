3147d006073df0a0ea22879a2b8cdeec6fd77a33
DragonVirus <limonster.li@gmail.com>
2019-02-14 15:54:44 +0630

    Add Google Calendar iFrame for Tempory

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home_wrapper">
-    <img src="../assets/art_in_yangon.png" alt="Art in Yangon" width="600px;">
+	<div class="header">
+		<img src="../assets/art_in_yangon.png" alt="Art in Yangon" width="200px">
+		<div class="btn_subscribe"><a href="https://calendar.google.com/calendar/render?cid=4judtsofjgctcqgvkgea9g76mc%40group.calendar.google.com" target="_blank">Subscribe to the Calendar</a> | <a href="https://calendar.google.com/calendar/ical/4judtsofjgctcqgvkgea9g76mc%40group.calendar.google.com/public/basic.ics">iCal</a></div>
+	</div>
+	<div class="calendar_wrapper">
+		<iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;src=4judtsofjgctcqgvkgea9g76mc%40group.calendar.google.com&amp;color=%23333333&amp;ctz=Asia%2FYangon" style="border-width:0" width="80%" height="600" frameborder="0" scrolling="no"></iframe>
+	</div>
   </div>
 </template>
